cc94fa64b9f066a6610d544bfdec86bc1031307a
Ranjan Jana <111300453+ranjan7586@users.noreply.github.com>
2025-12-17 11:29:02 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -10,7 +10,9 @@ connectDB();
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
-app.listen(PORT, () => {
+/*app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
+});*/
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
-
