1498b10391e3e6f706aaf3bb5972c4ff655a784e
ranjan7586 <111300453+ranjan7586@users.noreply.github.com>
2025-12-28 19:26:43 +0530

    cors config

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,31 @@
-import cors from "cors";
+import cors, { CorsOptions } from "cors";
 import express from "express";
 import cookieParser from "cookie-parser";
 
 // FIX 1: Add .js extension AND point to index.js explicitly (no folder imports allowed)
-import routes from "./routes"; 
+import routes from "./routes";
 
 // FIX 2: Add .js extension
-import { setLanguage } from "./middlewares/language.middleware"; 
+import { setLanguage } from "./middlewares/language.middleware";
 import { globalErrorHandler } from "./middlewares/error.middleware";
 
 const app = express();
+const whiteListURLs: string[] = ["http://localhost:5173"];
 
-app.use(cors());
+const corsOptions: CorsOptions = {
+  origin: (
+    origin: string | undefined,
+    callback: (err: Error | null, allow?: boolean) => void
+  ) => {
+      if ((origin && whiteListURLs.indexOf(origin) !== -1) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+};
+app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
 app.use(setLanguage);
